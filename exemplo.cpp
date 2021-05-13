96836fde30c2f362ba14b53b2b9cd3e6f7acad4f
Lucas Morais <lucas.morais446@hotmail.com>
2021-05-13 19:25:54 -0300

    Start & Finish function

--- exemplo.cpp
+++ exemplo.cpp
@@ -1,36 +1,21 @@
 #include <iostream>
 #include <list>
-#include <string>
 #include <pthread.h>
 #include "simone.h"
 
 using namespace std;
 
-void* OlaMundo(void* pPalavra){
-    int dta = *((int*) pPalavra);
 
-    for (int i = 0; i < dta; ++i){
-        cout << "Ola mundo" << endl;
-    }
-}
 
 int main(){
-    int paramentro_oimundo = 10;
+    // int paramentro_oimundo = 10;
 
-    Tarefa tarefa;
-    tarefa.funcao = OlaMundo;
-    tarefa.parametros = &paramentro_oimundo
+    // Tarefa tarefa;
+    // tarefa.funcao = OlaMundo;
+    // tarefa.parametros = (void*)&paramentro_oimundo;
 
     int pvs = 5;
     start(pvs);
-
-    tarefasProntas.push_back(tarefa);
-    tarefasProntas.push_back(tarefa);
-    tarefasProntas.push_back(tarefa);
-    tarefasProntas.push_back(tarefa);
-    tarefasProntas.push_back(tarefa);
-    tarefasProntas.push_back(tarefa);
-    tarefasProntas.push_back(tarefa);
-    
+    finish(pvs);
     return 0;
 }
--- simone.h
+++ simone.h
@@ -1,27 +1,48 @@
 #include <iostream>
 #include <list>
-#include <string>
 #include <pthread.h>
 
 using namespace std;
+
+void* OlaMundo(void* pPalavra){
+    int dta = *((int*) pPalavra);
+
+    for (int i = 0; i < dta; ++i){
+        cout << "Ola mundo" << endl;
+    }
+    return 0;
+}
 
 class Tarefa{
 public:
     int id;
     void* (*funcao) (void*);
-    list <void*> parametros;
+    void* parametros;
 };
 
 list <Tarefa> tarefasProntas;
 list <pthread_t> threads;
 
-int start(int m){
-    for (int i = 0; i < m; ++i){
-        pthread_t thread;
-        pthread_create(*thread, NULL, loop_que_itera, NULL);
-    }
+void* loop_que_itera(void*p) {
+    cout << "ITERANDO" << endl;
+    return 0;
 }
 
-void* loop_que_itera(void*p) {
-    
+int start(int m){
+    void* (*func)(void*);
+    func = loop_que_itera;
+    for (int i = 0; i < m; ++i){
+        pthread_t thread;
+        threads.push_back(thread);
+        pthread_create(&thread, NULL, func, NULL);
+    }
+    return 1;
+}
+
+int finish(int m){
+    for (int i = 0; i < m; ++i){
+        pthread_join(threads.front(), NULL);
+        threads.pop_front();
+    }
+    return 1;
 }
